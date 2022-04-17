f37aaec905ba1e8e7fe7b783236073c65568abad
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-16 21:59:38 -0700

    add Delete route

--- MessageBoard/Controllers/MessagesController.cs
+++ MessageBoard/Controllers/MessagesController.cs
@@ -54,7 +54,7 @@ namespace MessageBoard.Controllers
     // PUT is like POST in that it makes a change to the server. However, PUT changes existing information while POST creates new information. 
 
     // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-    // [HttpPut] annotation specifies that we'll determine which animal will be updated based on the id parameter in the URL.    
+    // [HttpPut] annotation specifies that we'll determine which message will be updated based on the id parameter in the URL.    
     [HttpPut("{id}")]
     public async Task<IActionResult> Put(int id, Message message)
     {
@@ -80,6 +80,26 @@ namespace MessageBoard.Controllers
           throw;
         }
       }
+
+      return NoContent();
+    }
+
+    // DELETE: api/Messages/5
+
+    // new [HttpDelete] annotation takes an id as a URL parameter just like our equivalent GET and PUT methods. Entity doesn't care whether it gets information from an API or a web application when manipulating the database.
+    // Remember that forms in HTML5 don't allow for PUT, PATCH or DELETE verbs. For that reason, we had to use HttpPost along with an ActionName like this: [HttpPost, ActionName("Delete")]. However, we aren't using HTML anymore and there are no such limitations with an API
+    // For that reason, we can use HttpPut and HttpDelete.
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMessage(int id)
+    {
+      var message = await _db.Messages.FindAsync(id);
+      if (message == null)
+      {
+        return NotFound();
+      }
+
+      _db.Messages.Remove(message);
+      await _db.SaveChangesAsync();
 
       return NoContent();
     }
