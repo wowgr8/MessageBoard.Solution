5ef5c87c9490f8a4cdddf37ab30e022986475180
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-16 21:29:00 -0700

    Seeded data

--- MessageBoard/Controllers/MessagesController.cs
+++ MessageBoard/Controllers/MessagesController.cs
@@ -3,3 +3,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MessageBoard.Models;
+
+namespace MessageBoard.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class MessagesController : ControllerBase
+  {
+    private readonly MessageBoardContext _db;
+
+    public MessagesController(MessageBoardContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/messages
+    [HttpGet]
+    // Get route needs to return an ActionResult of type IEnumerable<message>>. In our web applications, we didn't need to specify a type because we were always returning a view.
+    public async Task<ActionResult<IEnumerable<Message>>> Get()
+    {
+      return await _db.Messages.ToListAsync();
+    }
+
+    // POST api/messages
+    // Post route utilizes CreatedAtAction function so that it can end up returning the message object to the user as well as update the status code to 201 for "Created" rather than the default 200 OK
+    public async Task<ActionResult<Message>> Post(Message message)
+    {
+      _db.Messages.Add(message);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction("Post", new { id = message.MessageId }, message);
+    }
+  }
+}
