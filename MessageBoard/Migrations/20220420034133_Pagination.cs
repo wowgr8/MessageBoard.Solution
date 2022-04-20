e24ff80d760827ce29b5bf6ee2b9aca3e0a360c9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-04-19 20:46:41 -0700

    add migration for Pagination
    Co-authored-by: Wajma Niazi <w1niazi@hotmail.com>
    Co-Authored-by: Chris Nakayama <cnakayam@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

---
+++ MessageBoard/Migrations/20220420034133_Pagination.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MessageBoard.Migrations
+{
+    public partial class Pagination : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 5);
+
+            migrationBuilder.DropColumn(
+                name: "Pages",
+                table: "Messages");
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 4,
+                column: "Title",
+                value: "Bag Pipes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Pages",
+                table: "Messages",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 1,
+                column: "Pages",
+                value: 7);
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 2,
+                column: "Pages",
+                value: 10);
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 3,
+                column: "Pages",
+                value: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "MessageId",
+                keyValue: 4,
+                columns: new[] { "Pages", "Title" },
+                values: new object[] { 4, "Pipes" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "MessageId", "From", "Pages", "Title", "To" },
+                values: new object[] { 5, "Foolio", 22, "Binge Drinking 101", "Dinosaur" });
+        }
+    }
+}
